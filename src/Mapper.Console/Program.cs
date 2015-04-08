6545f539380f742ce00eb74285febb6680b1d594
unknown <heych@GBBLL3658.drl.local>
2015-04-08 13:15:20 +0100

    Demo via the service too

--- src/Mapper.Console/Program.cs
+++ src/Mapper.Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Fluent_Mapper.Base;
+using Fluent_Mapper.Services;
 
 namespace Mapper.Demo
 {
@@ -9,6 +10,8 @@ namespace Mapper.Demo
         {
             // generate a random entity
             var entity = new Entity();
+
+            Console.WriteLine("Directly mapping via Fluent method chaining");
 
             // pass it to the lite mapper
             var product = new Product().MapLite(entity);
@@ -25,6 +28,19 @@ namespace Mapper.Demo
             Console.WriteLine(fullProduct.Id);
             Console.WriteLine(fullProduct.Name);
             Console.WriteLine(fullProduct.Colour);
+
+            // via the service
+            Console.WriteLine("Via the service");
+            Console.WriteLine("Mapped to Lite");
+            var servicedProduct = new ProductService().GetLiteProduct(entity);
+
+            Console.WriteLine(servicedProduct.Id);
+
+            Console.WriteLine("Mapped to full");
+            var fullServicedProduct = new ProductService().GetFullProduct(entity);
+            Console.WriteLine(fullServicedProduct.Id);
+            Console.WriteLine(fullServicedProduct.Name);
+            Console.WriteLine(fullServicedProduct.Colour);
 
             // pause and wait for input before closing the window
             Console.ReadKey();
