74394faa63af75adefac5f594a46ba840f41c2a9
unknown <heych@GBBLL3658.drl.local>
2015-04-08 10:02:00 +0100

    Create random entities, Call the mappers in a demo project and ouput the results

--- src/Fluent Mapper/Base/Entity.cs
+++ src/Fluent Mapper/Base/Entity.cs
@@ -1,7 +1,21 @@
-﻿namespace Fluent_Mapper.Base
+﻿using System;
+
+namespace Fluent_Mapper.Base
 {
     public class Entity
     {
+        private readonly Random _random = new Random();
+
+        private readonly string[] _names = {"Product 1", "Product 2", "Product 3"};
+        private readonly string[] _colours = {"Red", "Blue", "Green", "Yellow"};
+
+        public Entity()
+        {
+            Attribute1 = _random.Next(0, 100000).ToString();
+            Attribute2 = _names[_random.Next(0, _names.Length)];
+            Attribute3 = _names[_random.Next(0, _colours.Length)];
+        }
+
         public string Attribute1 { get; set; }
         public string Attribute2 { get; set; }
         public string Attribute3 { get; set; }
--- src/Mapper.Console/Program.cs
+++ src/Mapper.Console/Program.cs
@@ -1,18 +1,28 @@
-﻿namespace Mapper.Console
+﻿using System;
+using Fluent_Mapper.Base;
+
+namespace Mapper.Demo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // generate a random product
+            // generate a random entity
+            var entity = new Entity();
 
             // pass it to the lite mapper
+            var product = new Product().MapLite(entity);
 
             // show output
+            Console.WriteLine(product.Id);
 
             // pass it through both mappers
+            var fullProduct = new Product().MapFull(entity);
 
             //show output
+            Console.WriteLine(fullProduct.Id);
+            Console.WriteLine(fullProduct.Name);
+            Console.WriteLine(fullProduct.Colour);
         }
     }
 }
