89ff95f0bdfce2a29d1065e49844d68b5a420f3e
unknown <heych@GBBLL3658.drl.local>
2015-04-08 09:05:38 +0100

    Implement the fluent method chaining in a service

--- src/Fluent Mapper/Base/Product.cs
+++ src/Fluent Mapper/Base/Product.cs
@@ -5,5 +5,25 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Colour { get; set; }
+
+        public Product Create
+        {
+            get { return new Product(); }
+        }
+
+        public Product MapLite(Entity entity)
+        {
+            Id = int.Parse(entity.Attribute1);
+
+            return this;
+        }
+
+        public Product MapFull(Entity entity)
+        {
+            Name = entity.Attribute2;
+            Colour = entity.Attribute3;
+
+            return this;
+        }
     }
 }
--- src/Fluent Mapper/Services/ProductService.cs
+++ src/Fluent Mapper/Services/ProductService.cs
@@ -1,6 +1,17 @@
-﻿namespace Fluent_Mapper.Services
+﻿using Fluent_Mapper.Base;
+
+namespace Fluent_Mapper.Services
 {
     public class ProductService
     {
+        public Product GetLiteProduct(Entity entity)
+        {
+            return new Product().MapLite(entity);
+        }
+
+        public Product GetFullProduct(Entity entity)
+        {
+            return new Product().MapLite(entity).MapFull(entity);
+        }
     }
 }
